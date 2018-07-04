ac71cac45df80ee3ed77d8dc0371221cf9daef5a
Jaehyun Nam <namjh@kaist.ac.kr>
2018-07-04 17:02:36 +0900

    minor updates

--- workloads/sender/config.sh
+++ workloads/sender/config.sh
@@ -3,7 +3,7 @@
 INTERFACE="enp12s0"
 IP_ADDR=`ifconfig $INTERFACE | grep "inet addr" | awk '{print $2}' | awk -F":" '{print $2}'`
 
+RECEIVER_NET="192.168.10.0"
+
 SENDER_IP="192.168.10.10"
 SENDER_GW="192.168.10.1"
-
-RECEIVER_NET="192.168.10.0"
